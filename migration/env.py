3560f11a833cbf8849750e90ea6e516343461b86
Alexandr1207 <andrianov-05@mail.ru>
2026-08-18 14:36:07 +0500

    feat: add Docker and Docker Compose setup with PostgreSQL

--- migration/env.py
+++ migration/env.py
@@ -8,14 +8,18 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app.database.database import Base
+from app.database.database import Base, engine
 import app.database.models
+import os
 
 target_metadata = Base.metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+db_url = os.getenv("DATABASE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -56,18 +60,9 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+def run_migrations_online():
+    # Вместо engine_from_config используйте ваш собственный engine из проекта:
+    connectable = engine
 
     with connectable.connect() as connection:
         context.configure(
